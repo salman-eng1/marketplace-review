0005a9534c5d96f111fbc6b7418cc50964752b2f
salman-eng1 <eng.salman2net@gmail.com>
2024-07-14 02:35:29 +0300

    fix git issue

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+
 import 'express-async-errors';
 import { CustomError, IAuthPayload, IErrorResponse, winstonLogger } from '@salman-eng1/marketplace-shared';
 import { Logger } from 'winston';
@@ -9,10 +10,12 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { verify } from 'jsonwebtoken';
 import compression from 'compression';
+
 import { checkConnection } from '@review/elasticsearch';
 import { appRoutes } from '@review/routes';
 import { createConnection } from '@review/queues/connection';
 import { Channel } from 'amqplib';
+
 
 const SERVER_PORT = 4007;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'reviewServer', 'debug');
